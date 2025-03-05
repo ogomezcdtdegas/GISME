ddd1fa577f8ae96762987b85ac776e3180e1f4b8
Manulex5000 <malfonso@cdtdegas.com>
2025-03-05 16:54:42 -0500

    dd

--- _AppCalc1/urls.py
+++ _AppCalc1/urls.py
@@ -2,11 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
-    # 📌 Ruta para la página de inicio
-    path('calc1/', views.indexCalc1, name='calc1'),
-]
-=======
     path('', views.index, name='calc1'),  # 📌 URL de Calc1
 ]
->>>>>>> origin/main
