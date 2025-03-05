207c5a09b36ee59c36e210a9384ac9926355fbb6
Manulex5000 <malfonso@cdtdegas.com>
2025-03-05 16:52:57 -0500

    solucion del merge

--- _AppCalc1/views.py
+++ _AppCalc1/views.py
@@ -2,11 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 
-<<<<<<< HEAD
 
-def indexCalc1(request):
-    return render(request, '_AppCalc1/index.html')
-=======
 def index(request):
     return render(request, '_AppCalc1/index.html')
->>>>>>> origin/main
