d25be1ff277268b8ec1a38e8bd1db0233498a877
Oscar <ogomez@cdtdegas.com>
2025-03-06 14:09:33 -0500

    actualizacion del home para crear equipos

--- _AppHome/models.py
+++ _AppHome/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Equipo(models.Model):
+    serial = models.CharField(max_length=50, unique=True, verbose_name="Serial")
+    sap = models.CharField(max_length=50, verbose_name="SAP")
+    marca = models.CharField(max_length=50, verbose_name="Marca")
+    fecha_registro = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de Registro")
+
+    def __str__(self):
+        return f"{self.serial} - {self.marca}"
