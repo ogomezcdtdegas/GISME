b2fae5379a8390e3a5271ef8ae135eb2b1a0c161
josefo14 <jfuentes@cdtdegas.com>
2025-03-06 07:33:51 -0500

    mis cambios

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from API_Calculo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home,name="home"),
+    path('calcular_incertidumbre/', views.calcular_incertidumbre, name='calcular_incertidumbre'),
 ]
