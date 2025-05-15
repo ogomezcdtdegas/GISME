f2e8a43e4ce9c20e4bf6676697e7e0f76c603626
Oscar <ogomez@cdtdegas.com>
2025-05-15 17:00:28 -0500

    se adiciona javascript en fluxpro prop. para capturar datos del form y recibir datos de libreria, falta renderizar frontend con datos calculados

--- _AppHerramientas/urls.py
+++ _AppHerramientas/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import FluxPro_view
+from .views import FluxPro_view, FluxCalcProp_view
 
 urlpatterns = [
     path("fluxpro/", FluxPro_view, name="fluxpro"),
+    path("FluxCalcProp/", FluxCalcProp_view.as_view(), name="FluxCalcProp"),
 ]
