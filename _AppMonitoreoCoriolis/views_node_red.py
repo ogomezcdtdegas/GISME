26052282e58c98e7dd3a645466e77832d46c2ae4
Oscar <ogomez@cdtdegas.com>
2025-07-24 15:15:31 -0500

    se realiza refactorización del view de nored que recibe en la app de moniotero los datos, para que use el repoGenerico en la creación

--- _AppMonitoreoCoriolis/views_node_red.py
+++ _AppMonitoreoCoriolis/views_node_red.py
@@ -1,15 +1,16 @@
 
-from rest_framework.views import APIView
+
 from rest_framework.response import Response
-from rest_framework import status
-from django.conf import settings
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from .models import NodeRedData
-from repoGenerico.views_base import BasicNodeRedAuthMixin
+from .serializers import NodeRedDataSerializer
+from repoGenerico.views_base import BasicNodeRedAuthMixin, BaseCreateView
 
 @method_decorator(csrf_exempt, name='dispatch')
-class NodeRedReceiverView(BasicNodeRedAuthMixin, APIView):
+class NodeRedReceiverView(BasicNodeRedAuthMixin, BaseCreateView):
+    model = NodeRedData
+    serializer_class = NodeRedDataSerializer
     authentication_classes = []
     permission_classes = []
 
@@ -17,11 +18,4 @@ class NodeRedReceiverView(BasicNodeRedAuthMixin, APIView):
         auth_error = self.check_basic_auth(request)
         if auth_error:
             return auth_error
-
-        try:
-            data = request.data
-            NodeRedData.objects.create(sensor=data.get('sensor'), valor=data.get('valor'))
-            print("Datos recibidos desde Node-RED:", data)
-            return Response({'success': True, 'received': data})
-        except Exception as e:
-            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        return super().post(request, *args, **kwargs)
