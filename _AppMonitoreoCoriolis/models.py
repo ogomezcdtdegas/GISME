19d45a5e039c0759c89890e962c3833c54cde3e3
ogomezcdtdegas <ogomez@cdtdegas.com>
2025-09-19 16:14:54 -0500

    Actualización de modelo de nodeRedIOT

--- _AppMonitoreoCoriolis/models.py
+++ _AppMonitoreoCoriolis/models.py
@@ -2,11 +2,25 @@ from django.db import models
 from _AppCommon.models import BaseModel
 
 class NodeRedData(BaseModel):
-    sensor = models.CharField(max_length=100)
-    valor = models.FloatField()
+    tag_Sistema = models.CharField(max_length=100, null=True, blank=True)
+    volumen_total = models.FloatField(null=True, blank=True)
+    volumen_fwd = models.FloatField(null=True, blank=True)
+    volumen_rev = models.FloatField(null=True, blank=True)
+    masa_total = models.FloatField(null=True, blank=True)
+    masa_fwd = models.FloatField(null=True, blank=True)
+    masa_rev = models.FloatField(null=True, blank=True)
+    densidad = models.FloatField(null=True, blank=True)
+    volumen_60f = models.FloatField(null=True, blank=True)
+    grav_spec_60f = models.FloatField(null=True, blank=True)
+    caudal_rate = models.FloatField(null=True, blank=True)
+    mass_rate = models.FloatField(null=True, blank=True)
+    temperatura_coriolis = models.FloatField(null=True, blank=True)
+    temperatura_diagnostico = models.FloatField(null=True, blank=True)
+    temperatura_redundante = models.FloatField(null=True, blank=True)
+    presion = models.FloatField(null=True, blank=True)
 
     def __str__(self):
-        return f"{self.sensor}: {self.valor}"
+        return f"{self.tag_Sistema}"
 
 # Create your models here.
 
--- _AppMonitoreoCoriolis/serializers.py
+++ _AppMonitoreoCoriolis/serializers.py
@@ -4,4 +4,9 @@ from .models import NodeRedData
 class NodeRedDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = NodeRedData
-        fields = ['id', 'sensor', 'valor', 'created_at']
+        fields = [
+            'id', 'tag_Sistema', 'volumen_total', 'volumen_fwd', 'volumen_rev',
+            'masa_total', 'masa_fwd', 'masa_rev', 'densidad', 'volumen_60f',
+            'grav_spec_60f', 'caudal_rate', 'mass_rate', 'temperatura_coriolis',
+            'temperatura_diagnostico', 'temperatura_redundante', 'presion', 'created_at'
+        ]
