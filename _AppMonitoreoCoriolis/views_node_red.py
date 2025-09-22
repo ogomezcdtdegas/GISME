91df066416e24f7fa367724e6b9d21f48027021c
ogomezcdtdegas <ogomez@cdtdegas.com>
2025-09-22 16:00:06 -0500

    Actualizacion envio iot

--- _AppMonitoreoCoriolis/views_node_red.py
+++ _AppMonitoreoCoriolis/views_node_red.py
@@ -1,11 +1,10 @@
-
-
 from rest_framework.response import Response
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from .models import NodeRedData
 from .serializers import NodeRedDataSerializer
 from repoGenerico.views_base import BasicNodeRedAuthMixin, BaseCreateView
+from _AppComplementos.models import Sistema
 
 @method_decorator(csrf_exempt, name='dispatch')
 class NodeRedReceiverView(BasicNodeRedAuthMixin, BaseCreateView):
@@ -18,4 +17,12 @@ class NodeRedReceiverView(BasicNodeRedAuthMixin, BaseCreateView):
         auth_error = self.check_basic_auth(request)
         if auth_error:
             return auth_error
+
+        mac_gateway = request.data.get("mac_gateway")
+        if not mac_gateway or not Sistema.objects.filter(sistema_id=mac_gateway).exists():
+            return Response(
+                {"detail": "mac_gateway no registrado como sistema_id en sistemas."},
+                status=400
+            )
+
         return super().post(request, *args, **kwargs)
