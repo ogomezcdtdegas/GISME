40bfa7e707e9f10bb9231ae4c5b6821e62a72906
ogomezcdtdegas <ogomez@cdtdegas.com>
2025-09-22 07:21:14 -0500

    ajuste de variables nodered en inglés

--- _AppMonitoreoCoriolis/models.py
+++ _AppMonitoreoCoriolis/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from _AppCommon.models import BaseModel
-
+"""
 class NodeRedData(BaseModel):
     tag_Sistema = models.CharField(max_length=100, null=True, blank=True)
     volumen_total = models.FloatField(null=True, blank=True)
@@ -21,6 +21,27 @@ class NodeRedData(BaseModel):
 
     def __str__(self):
         return f"{self.tag_Sistema}"
+"""
 
+class NodeRedData(BaseModel):
+    tag_system = models.CharField(max_length=100, null=True, blank=True)
+    total_volume = models.FloatField(null=True, blank=True)
+    forward_volume = models.FloatField(null=True, blank=True)
+    reverse_volume = models.FloatField(null=True, blank=True)
+    total_mass = models.FloatField(null=True, blank=True)
+    forward_mass = models.FloatField(null=True, blank=True)
+    reverse_mass = models.FloatField(null=True, blank=True)
+    density = models.FloatField(null=True, blank=True)
+    volume_60f = models.FloatField(null=True, blank=True)
+    specific_gravity_60f = models.FloatField(null=True, blank=True)
+    flow_rate = models.FloatField(null=True, blank=True)
+    mass_rate = models.FloatField(null=True, blank=True)
+    coriolis_temperature = models.FloatField(null=True, blank=True)
+    diagnostic_temperature = models.FloatField(null=True, blank=True)
+    redundant_temperature = models.FloatField(null=True, blank=True)
+    pressure = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.tag_system}"
 # Create your models here.
 
