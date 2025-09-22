9c9db0b35ca43568c18d0d0e1f73931fbe3fc951
ogomezcdtdegas <ogomez@cdtdegas.com>
2025-09-22 09:36:21 -0500

    Se raliza correción del serializador del AppMonitoreoCoriolis

--- _AppMonitoreoCoriolis/models.py
+++ _AppMonitoreoCoriolis/models.py
@@ -1,47 +1,26 @@
 from django.db import models
 from _AppCommon.models import BaseModel
-"""
-class NodeRedData(BaseModel):
-    tag_Sistema = models.CharField(max_length=100, null=True, blank=True)
-    volumen_total = models.FloatField(null=True, blank=True)
-    volumen_fwd = models.FloatField(null=True, blank=True)
-    volumen_rev = models.FloatField(null=True, blank=True)
-    masa_total = models.FloatField(null=True, blank=True)
-    masa_fwd = models.FloatField(null=True, blank=True)
-    masa_rev = models.FloatField(null=True, blank=True)
-    densidad = models.FloatField(null=True, blank=True)
-    volumen_60f = models.FloatField(null=True, blank=True)
-    grav_spec_60f = models.FloatField(null=True, blank=True)
-    caudal_rate = models.FloatField(null=True, blank=True)
-    mass_rate = models.FloatField(null=True, blank=True)
-    temperatura_coriolis = models.FloatField(null=True, blank=True)
-    temperatura_diagnostico = models.FloatField(null=True, blank=True)
-    temperatura_redundante = models.FloatField(null=True, blank=True)
-    presion = models.FloatField(null=True, blank=True)
-
-    def __str__(self):
-        return f"{self.tag_Sistema}"
-"""
 
 class NodeRedData(BaseModel):
-    tag_Sistema = models.CharField(max_length=100, null=True, blank=True)
-    volumen_total = models.FloatField(null=True, blank=True)
-    volumen_fwd = models.FloatField(null=True, blank=True)
-    volumen_rev = models.FloatField(null=True, blank=True)
-    masa_total = models.FloatField(null=True, blank=True)
-    masa_fwd = models.FloatField(null=True, blank=True)
-    masa_rev = models.FloatField(null=True, blank=True)
-    densidad = models.FloatField(null=True, blank=True)
-    volumen_60f = models.FloatField(null=True, blank=True)
-    grav_spec_60f = models.FloatField(null=True, blank=True)
-    caudal_rate = models.FloatField(null=True, blank=True)
+    tag_system = models.CharField(max_length=100, null=True, blank=True)
+    total_volume = models.FloatField(null=True, blank=True)
+    forward_volume = models.FloatField(null=True, blank=True)
+    reverse_volume = models.FloatField(null=True, blank=True)
+    total_mass = models.FloatField(null=True, blank=True)
+    forward_mass = models.FloatField(null=True, blank=True)
+    reverse_mass = models.FloatField(null=True, blank=True)
+    density = models.FloatField(null=True, blank=True)
+    volume_60f = models.FloatField(null=True, blank=True)
+    specific_gravity_60f = models.FloatField(null=True, blank=True)
+    flow_rate = models.FloatField(null=True, blank=True)
     mass_rate = models.FloatField(null=True, blank=True)
-    temperatura_coriolis = models.FloatField(null=True, blank=True)
-    temperatura_diagnostico = models.FloatField(null=True, blank=True)
-    temperatura_redundante = models.FloatField(null=True, blank=True)
-    presion = models.FloatField(null=True, blank=True)
+    coriolis_temperature = models.FloatField(null=True, blank=True)
+    diagnostic_temperature = models.FloatField(null=True, blank=True)
+    redundant_temperature = models.FloatField(null=True, blank=True)
+    pressure = models.FloatField(null=True, blank=True)
 
     def __str__(self):
-        return f"{self.tag_Sistema}"
+        return f"{self.tag_system}"
+
 # Create your models here.
 
--- _AppMonitoreoCoriolis/serializers.py
+++ _AppMonitoreoCoriolis/serializers.py
@@ -4,10 +4,5 @@ from .models import NodeRedData
 class NodeRedDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = NodeRedData
-        fields = [
-            'id', 'tag_Sistema', 'volumen_total', 'volumen_fwd', 'volumen_rev',
-            'masa_total', 'masa_fwd', 'masa_rev', 'densidad', 'volumen_60f',
-            'grav_spec_60f', 'caudal_rate', 'mass_rate', 'temperatura_coriolis',
-            'temperatura_diagnostico', 'temperatura_redundante', 'presion', 'created_at'
-        ]
+        fields = '__all__'
 
