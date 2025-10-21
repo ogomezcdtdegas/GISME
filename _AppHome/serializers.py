5c579041e997f779c8357e37f9bcb217ea5c2141
ogomezcdtdegas <ogomez@cdtdegas.com>
2025-10-21 09:15:52 -0500

    default

--- _AppHome/serializers.py
+++ _AppHome/serializers.py
@@ -1,9 +1,1 @@
-from rest_framework import serializers
-from .models import Equipo
 
-class EquipoSerializer(serializers.ModelSerializer):
-    created_at = serializers.DateTimeField(format="%d-%m-%Y %H:%M", required=False, read_only=True)  
-
-    class Meta:
-        model = Equipo
-        fields = ["id", "serial", "sap", "marca", "created_at"]  # 🔹 Agregar "id"
--- _AppHome/views/views_Equipo/Commands/UpdateEquipoCommand/UpdateEquipoCommand.py
+++
@@ -1,8 +0,0 @@
-from repoGenerico.views_base import BaseRetrieveUpdateView
-from .....models import Equipo
-from .....serializers import EquipoSerializer
-
-# 🔹 Edición
-class editarEquipo(BaseRetrieveUpdateView):
-    model = Equipo
-    serializer_class = EquipoSerializer
