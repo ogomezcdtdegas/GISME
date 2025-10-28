e3499b0ff2bdf524f410e956a2ccaf9e6520d439
ogomezcdtdegas <ogomez@cdtdegas.com>
2025-10-27 20:46:41 -0500

    rama que tiene la versión sin login de azure y parte de complementos de creación de productos etc

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,7 @@ DEBUG = False
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS_PROD', 'tudominio.com').split(',')
 SESSION_COOKIE_AGE = 1800
 
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -26,5 +26,5 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+'''
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
