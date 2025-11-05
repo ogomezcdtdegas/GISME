0a6ba6cd8282a2e818cebc350d8d9b7580bd3654
ogomezcdtdegas <ogomez@cdtdegas.com>
2025-11-05 13:59:50 -0500

    Sincronización development

--- config/settings/development.py
+++ config/settings/development.py
@@ -5,10 +5,6 @@ DEBUG = True
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS_DEV', '127.0.0.1,localhost,tudominio.com').split(',')
 SESSION_COOKIE_AGE = 1200
 
-<<<<<<< HEAD
-=======
-
->>>>>>> msalonlyv43
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -19,17 +15,6 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT'),
     }
 }
-<<<<<<< HEAD
 
-=======
-'''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-'''
->>>>>>> msalonlyv43
 # Habilitar Swagger/Schema solo en desarrollo
 REST_FRAMEWORK['DEFAULT_SCHEMA_CLASS'] = 'drf_spectacular.openapi.AutoSchema'
