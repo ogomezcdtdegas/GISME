419cdbc3cdf001bcacdfaaf18616c604371aa195
ogomezcdtdegas <ogomez@cdtdegas.com>
2025-11-10 11:54:21 -0500

    actualizacion config header

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,2 +1,6 @@
 def post_request(worker, req, environ, resp):
-    resp.headers['Server'] = 'undisclosed'
+    # resp.headers is a list of tuples in some environments (like Azure)
+    # Remove any existing 'Server' header
+    resp.headers = [(k, v) for (k, v) in resp.headers if k.lower() != 'server']
+    # Add your custom header
+    resp.headers.append(('Server', 'undisclosed'))
